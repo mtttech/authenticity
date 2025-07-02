73d3e9b5b9f831b778a8d9b14aafc3b8da50c416
Marcus T Taylor <mtaylor9754@hotmail.com>
2025-07-02 03:38:36 -0400

    First commit.

---
+++ src/authenticity/models.py
@@ -0,0 +1,32 @@
+"""
+models.py
+Author:     Marcus T Taylor
+Created:    26.11.23
+Modified:   11.10.24
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base
+
+engine = create_engine("sqlite:///authenticity/authenticity.db")
+Base = declarative_base()
+
+
+class Exercises(Base):
+    __tablename__ = "exercises"
+    exercise_id = Column(Integer, primary_key=True)
+    workout_id = Column(Integer)
+    exercise_name = Column(String)
+    exercise_set = Column(Integer)
+    exercise_reps = Column(Integer)
+    exercise_weight = Column(Integer)
+
+
+class Workouts(Base):
+    __tablename__ = "workouts"
+    workout_id = Column(Integer, primary_key=True)
+    workout_title = Column(String)
+    workout_date = Column(String)
+
+
+Base.metadata.create_all(engine)
