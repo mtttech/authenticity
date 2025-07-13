ca0a341212c4447d78a6ab576f35fee430e01280
Marcus T Taylor <mtaylor9754@hotmail.com>
2025-07-12 22:54:20 -0400

    Code clean up. Now using rich Prompt.

--- src/authenticity/cli.py
+++ src/authenticity/cli.py
@@ -2,7 +2,7 @@
 cli.py
 Author:     Marcus T Taylor
 Created:    23.11.23
-Modified:   10.07.25
+Modified:   12.07.25
 """
 
 from datetime import datetime
@@ -10,6 +10,7 @@ from typing import Any
 
 import rich_click as click
 from rich.console import Console
+from rich.prompt import Confirm, Prompt
 from rich.table import Table
 
 from authenticity.exercises import get_exercises_by_group, get_muscle_groups
@@ -18,14 +19,14 @@ from authenticity.models import Exercises, Workouts, engine
 console = Console(tab_size=4, width=80)
 
 
-def io(choices: list[str] | int) -> list[str]:
+def io(choices: list[str] | int) -> str:
     """Captures user input from the console.
 
     Args:
         choices (list[str]|int): List of choices or max value in a range of numbers.
 
     Returns:
-        list[str]: A list of the user's responses."""
+        str: Returns the user's response."""
 
     def first_and_last(choices: dict[int, Any]) -> tuple[int, int]:
         indexes = list(choices.keys())
@@ -57,10 +58,11 @@ def io(choices: list[str] | int) -> list[str]:
     except (KeyError, TypeError, ValueError):
         return io(choices)
 
-    return selections
+    return selections[0]
 
 
 @click.group()
+@click.version_option()
 @click.pass_context
 def cli(ctx):
     ctx.ensure_object(dict)
@@ -72,9 +74,8 @@ def add(ctx) -> None:
     def create_workout():
         from sqlalchemy import insert
 
-        workout_title = click.prompt(
-            "Name your workout session (i.e: My Meditation Session, etc).",
-            prompt_suffix=" ",
+        workout_title = Prompt.ask(
+            "Name your workout session (i.e: My Meditation Session, etc)."
         )
         # TODO: Add something to track the workout's duration, perhaps? >:(
         # TODO: Functional but lets clean this system up a lil bit.
@@ -87,34 +88,22 @@ def add(ctx) -> None:
             )
             conn.commit()
 
-            num_of_exercises = click.prompt(
-                "How many exercises were performed?",
-                prompt_suffix=" ",
-                type=int,
-            )
+            num_of_exercises = int(Prompt.ask("How many exercises were performed?"))
             # Ask for workout info for each exercise
             for _ in range(1, num_of_exercises + 1):
                 console.print("Enter the targeted body part.")
-                muscle_group = io(get_muscle_groups())[0]
+                muscle_group = io(get_muscle_groups())
 
                 console.print("Enter the exercise performed.")
-                exercise = io(get_exercises_by_group(muscle_group))[0]
+                exercise = io(get_exercises_by_group(muscle_group))
 
-                num_of_sets = click.prompt(
-                    "How many sets were performed?",
-                    prompt_suffix=" ",
-                    type=int,
-                )
-                weight = click.prompt(
-                    "How much weight was used?",
-                    prompt_suffix=" ",
-                    type=int,
-                )
+                num_of_sets = int(Prompt.ask("How many sets were performed?"))
+                weight = int(Prompt.ask("How much weight was used?"))
                 for set_number in range(1, num_of_sets + 1):
-                    num_of_reps = click.prompt(
-                        f"How many reps were performed for set {set_number}?",
-                        prompt_suffix=" ",
-                        type=int,
+                    num_of_reps = int(
+                        Prompt.ask(
+                            f"How many reps were performed for set {set_number}?"
+                        )
                     )
                     conn.execute(
                         insert(Exercises).values(
@@ -138,13 +127,14 @@ def add(ctx) -> None:
 @click.option("--wid", required=True, help="Workout ID number to delete.")
 @click.pass_context
 def delete(ctx, wid):
-    from sqlalchemy import delete
+    if Confirm.ask(f"Do you want to delete WID {wid}?"):
+        from sqlalchemy import delete
 
-    with engine.connect() as conn:  # pyright: ignore[reportGeneralTypeIssues]
-        conn.execute(delete(Workouts).where(Workouts.workout_id == wid))
-        conn.execute(delete(Exercises).where(Exercises.workout_id == wid))
-        conn.commit()
-        console.print(f"Deleted WID {wid}.")
+        with engine.connect() as conn:  # pyright: ignore[reportGeneralTypeIssues]
+            conn.execute(delete(Workouts).where(Workouts.workout_id == wid))
+            conn.execute(delete(Exercises).where(Exercises.workout_id == wid))
+            conn.commit()
+            console.print(f"Deleted WID {wid}.")
 
 
 @cli.command("view", help="View a workout.")
